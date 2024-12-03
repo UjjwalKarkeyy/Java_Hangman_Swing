779c779ae4c4701a5c3d876f158a1b794de0d0c6
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2024-12-03 07:42:08 +0545

    custom-title-bar-not-showing

--- Hangman.java
+++ Hangman.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -10,12 +10,38 @@ public class Hangman
         Hangman h1 = new Hangman();
 
         // Create JFrame class object and providing basic layout
-        JFrame frame = new JFrame("Hangman");
+        String titleText = "Hangman";
+        JFrame frame = new JFrame();
+        frame.setUndecorated(true); // Remove default title bar
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
-        frame.setLayout(null);
         frame.getContentPane().setBackground(Color.CYAN);
+
+        /*
+            CREATING CUSTOM TITLEPANEL
+        */
+
+        JPanel titlePanel = new JPanel();
+        
+        // Flow layout basically helps to deal with the x-axis i.e., the row. It works in a way that if a whole row is occupied then it will simply flow to the next row.
+        titlePanel.setLayout(new FlowLayout(FlowLayout.LEFT)); 
+        titlePanel.setBackground(Color.WHITE);
+        
+        /*
+            Creating TITLEPANEL label and adding it to the title panel
+        */        
+
+        JLabel title = new JLabel();
+        title.setText(titleText);
+        title.setFont(new Font("Arial", Font.BOLD, 18));
+        title.setForeground(Color.BLACK);
+        titlePanel.add(title);
+
+        /*
+            BUTTONS START HERE 
+        */
 
         // Initializing button numbers
         int buttonNumbers = 2;
@@ -25,19 +51,45 @@ public class Hangman
 
         // Creating play button
         b[0] = new JButton("Play");
-        b[0].setBackground(Color.GREEN);
+        b[0].setBackground(new Color(0,95,115));
+        b[0].setForeground(Color.WHITE);
         b[0].setBounds(345,250,64,30);
         
         // Creating host button
         b[1] = new JButton("Host");
-        b[1].setBackground(Color.GREEN);
-        b[1].setBounds(342,215,70,30);
+        b[1].setBackground(new Color(0,95,115));
+        b[1].setForeground(Color.WHITE);
+        b[1].setBounds(342,210,70,30);
 
-        // Automatically adding every button to the frame
+        /*
+            LABELS START HERE 
+        */
+
+        int labelNumbers = 1;
+        JLabel[] l = new JLabel[labelNumbers];
+
+        l[0] = new JLabel();
+        l[0].setText("Welcome to Hangman!");
+        l[0].setBounds(236, 100, 500, 30);
+        l[0].setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+
+        /*
+            AUTOMATICALLY ADDING  
+        */
+
+        // Automatically adding every buttons to the frame
         for(int i = 0; i < buttonNumbers; i++)
         {
             frame.add(b[i]);
         }
+        
+        // Automatically adding every labels to the frame
+        for(int i = 0; i < labelNumbers; i++)
+        {
+            frame.add(l[i]);
+        }
+
+        frame.add(titlePanel, BorderLayout.NORTH);
 
         frame.setVisible(true);
     }
