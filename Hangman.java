bbde96df58565f07538a02f4686350d1c2b4abea
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2024-12-04 06:56:04 +0545

    button_icons_added_needs resizing_&_relocating

--- Hangman.java
+++ Hangman.java
@@ -11,13 +11,14 @@ public class Hangman
 
         // Create JFrame class object and providing basic layout
         String titleText = "Hangman";
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame(titleText);
         frame.setLayout(null);
+        frame.setLayout(new BorderLayout());
         frame.setUndecorated(true); // Remove default title bar and border
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
-        frame.getContentPane().setBackground(Color.CYAN);
+        frame.getContentPane().setBackground(new Color(30,30,30));
 
         /*
             CREATING CUSTOM TITLEPANEL
@@ -40,11 +41,32 @@ public class Hangman
         titlePanel.add(title);
 
         /*
+            IMAGES ARE LOADED HERE 
+        */
+
+        ImageIcon mini = new ImageIcon("Icon_Images/minimize_icon.png");
+        ImageIcon close = new ImageIcon("Icon_Images/close_icon.png");
+
+        /*
+            RESIZING THE IMAGES LOADED 
+        */
+
+        Image temp_mini = mini.getImage();
+        Image temp_close = close.getImage();
+
+        Image re_mini = temp_mini.getScaledInstance(17, 17, 0);
+        Image re_close = temp_close.getScaledInstance(25, 25, 0);
+
+        ImageIcon mini_icon = new ImageIcon(re_mini); 
+        ImageIcon close_icon = new ImageIcon(re_close); 
+
+
+        /*
             BUTTONS START HERE 
         */
 
         // Initializing button numbers
-        int buttonNumbers = 2;
+        int buttonNumbers = 4;
 
         // Initializing JButton class array object
         JButton[] b = new JButton[buttonNumbers];
@@ -60,6 +82,14 @@ public class Hangman
         b[1].setBackground(new Color(0,95,115));
         b[1].setForeground(Color.WHITE);
         b[1].setBounds(342,210,70,30);
+
+        // Creating minimize button
+        b[2] = new JButton(mini_icon);
+        b[2].setBounds(342,0,70,30);
+
+        // Creating cross button
+        b[3] = new JButton(close_icon);
+        b[3].setBounds(442,0,100,50);
 
         /*
             LABELS START HERE 
@@ -89,7 +119,7 @@ public class Hangman
             frame.add(l[i]);
         }
 
-        frame.add(titlePanel, BorderLayout.SOUTH);
+        frame.add(titlePanel, BorderLayout.NORTH);
 
         frame.setVisible(true);
     }
